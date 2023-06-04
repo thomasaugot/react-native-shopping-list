0f7ca8db754d613521f76e387f905bd50fb88539
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-04 13:54:02 +0200

    initalizing app, adding input element and validation button

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [product, setProduct] = useState("");
+  const [myProducts, setMyProducts] = useState([]);
+
+  const inputHandler = (value) => {
+    setProduct(value);
+  };
+
+  const submitHandler = () => {
+    console.log(product);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nouveau Produit"
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button title="Valider" onPress={submitHandler} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 40,
+    paddingTop: 60,
+  },
+  inputContainer: { flexDirection: "row" },
+  textInput: {
+    borderColor: "grey",
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 9,
   },
 });
